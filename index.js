f78312e2098a69de0b03c6e944ff5f49f2b760dd
Karina Pliego <karinapliegovergara@gmail.com>
2023-11-05 17:17:10 -0600

    Working fetch

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-document.addEventListener("DOMContentLoaded", () => console.log("We're connected!"));
+document.addEventListener("DOMContentLoaded", () => fetchData);
+
+function fetchData() {
+    fetch('file:///Users/karinapliego/Development/code/phase-1/project-1/index.html')
+    .then((resp) => resp.json())
+    .then((data) => console.log(data))
+}
+console.log(fetchData);
